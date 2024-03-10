518e3ddeea149fab79d449d20fa857254bf948f7
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-03-09 21:43:00 -0500

    Initial Commit

---
+++ SwitchColorDemo/SwitchColorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchColorDemoApp.swift
+//  SwitchColorDemo
+//
+//  Created by Leonard McCook-Carr on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwitchColorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
